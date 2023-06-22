461b11bd16e912cee1423b2db85e9f21eec9cfed
Ronald803 <ronaldblancobalboa@gmail.com>
2023-06-22 14:24:26 -0400

    cors added

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express   = require('express');
+const cors      = require('cors');
 const config    = require('./config');
 const app       = express();
-const router    = require('./network/router')
+const router    = require('./network/router');
 const connectDB = require('./db')
 
 connectDB(config.dbUrl);
+app.use( cors() );
 app.use(express.json());
 router(app);
 
